b0a864979fdbe687bde0a93e8f379d68190eaa01
Đỗ Khắc Gia Khoa <dokhacgiakhoa666@gmail.com>
2025-11-01 07:46:36 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/asm-rmwd">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/asm-rmwd">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+reportWebVitals();
